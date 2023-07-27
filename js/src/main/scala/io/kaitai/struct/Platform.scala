f98d02ca3a2ce76f78b759fb6a6475df864d6999
Ryan Delaney <vitineth@gmail.com>
2023-07-27 16:46:45 +0100

    Adding platform specific markdown to html parsers with Platform impl

--- js/src/main/scala/io/kaitai/struct/Platform.scala
+++ js/src/main/scala/io/kaitai/struct/Platform.scala
@@ -1,5 +1,11 @@
 package io.kaitai.struct
 
+import com.karasiq.markedjs.Marked
+
 object Platform {
     def toUpperLocaleInsensitive(s: String) = s.toUpperCase()
+
+  def markdownToHtml(s: String): String = {
+    Marked(s)
+  }
 }
--- jvm/src/main/scala/io/kaitai/struct/Platform.scala
+++ jvm/src/main/scala/io/kaitai/struct/Platform.scala
@@ -1,7 +1,17 @@
 package io.kaitai.struct
 
 import java.util.Locale
+import org.commonmark.parser.Parser
+import org.commonmark.renderer.html.HtmlRenderer
 
 object Platform {
-    def toUpperLocaleInsensitive(s: String) = s.toUpperCase(Locale.ROOT)
+  def toUpperLocaleInsensitive(s: String) = s.toUpperCase(Locale.ROOT)
+
+  def markdownToHtml(s: String): String = {
+    val parser = Parser.builder().build()
+    val document = parser.parse(s)
+    val renderer = HtmlRenderer.builder().build()
+    renderer.render(document)
+  }
+
 }
